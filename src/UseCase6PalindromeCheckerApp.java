6a36d96b37da1037e883e1ae98d9e2196f2c1814
ditodileep050 <dd9834@srmist.edu.in>
2026-02-25 09:37:38 +0530

    use case 6 of palindromechecker

---
+++ src/UseCase6PalindromeCheckerApp.java
@@ -0,0 +1,39 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class UseCase6PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Define input string
+        String input = "civic";
+
+        // Create Queue (FIFO)
+        Queue<Character> queue = new LinkedList<>();
+
+        // Create Stack (LIFO)
+        Stack<Character> stack = new Stack<>();
+
+        // Insert characters into both structures
+        for (char c : input.toCharArray()) {
+            queue.add(c);      // Enqueue
+            stack.push(c);     // Push
+        }
+
+        // Flag to track palindrome
+        boolean isPalindrome = true;
+
+        // Compare dequeue and pop
+        while (!queue.isEmpty()) {
+            if (!queue.remove().equals(stack.pop())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
